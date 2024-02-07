dd0164c73b116b1de6dcac0a3b115d36d1546b10
asukamekaru98 <asuka.m2298oki@gmail.com>
2024-02-07 14:06:56 +0900

    QRリーダー完成

--- BarcodeReader/app/build.gradle.kts
+++ BarcodeReader/app/build.gradle.kts
@@ -1,3 +1,5 @@
+//import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -33,11 +35,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+
+    viewBinding {
+        enable = true
+    }
+
 }
 
 dependencies {
 
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -45,4 +56,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.google.zxing:core:3.4.1")                  //ライブラリダウンロード
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")  //ライブラリダウンロード
+
 }
--- BarcodeReader/app/src/main/java/com/websarva/wings/android/barcodereader/MainActivity.kt
+++ BarcodeReader/app/src/main/java/com/websarva/wings/android/barcodereader/MainActivity.kt
@@ -1,47 +1,100 @@
 package com.websarva.wings.android.barcodereader
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+import com.websarva.wings.android.barcodereader.databinding.ActivityMainBinding
+
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // ScanContractの結果を受け取る
-        val barcodeLauncher = registerForActivityResult(
-            ScanContract()
-        ) { result ->
-            if (result.contents == null) {
-                // ここでQRコードを読み取れなかった場合の処理を書く
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG)
-                    .show()
+
+    private val requestPremissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                showCamera()
             } else {
-                // ここでQRコードを読み取れた場合の処理を書く
-                // ここではトーストに結果を表示するだけ
-                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG)
-                    .show()
+
             }
         }
 
-        // QRコードリーダーの立ち上げ
-        fun onButtonClick() {
-            // 縦画面に固定
-            val options = ScanOptions().setOrientationLocked(false)
-            barcodeLauncher.launch(options)
-        }
 
-        super.onCreate(savedInstanceState)
-        setContent {
-            QRCodeReaderTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    // ボタンタップで起動
-                    TextButton(onClick = { onButtonClick() }) {
-                        Text(text = "QRCodeReaderを起動する")
-                    }
+    private lateinit var binding: ActivityMainBinding
+
+    private val scanLauncher =
+        registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
+            run{
+                if (result.contents == null) {
+                    Toast.makeText(this,"Cancelled",Toast.LENGTH_SHORT).show()
+                } else {
+                    setResult(result.contents)
                 }
             }
         }
+
+
+
+    private fun setResult(string: String){
+        binding.textResult.text = string
     }
+
+    private fun showCamera() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan QR Code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        options.setOrientationLocked(false)
+
+        scanLauncher.launch(options)
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBinding()
+        initViews()
+        //setContentView(R.layout.activity_main)
+
+    }
+
+    private fun initViews() {
+        binding.fab.setOnClickListener {
+            checkPremissionCamrea(this)
+        }
+    }
+
+    private fun checkPremissionCamrea(context: Context) {
+        if (ContextCompat.checkSelfPermission(
+                context,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            showCamera()
+        } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+            Toast.makeText(context, "CAMERA premission required", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPremissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
 }
